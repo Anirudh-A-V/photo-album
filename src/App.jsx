be968dd6e60b6c6f73fb8a6f3b94702093af4cdc
Anirudh A V <anirudh.av02@gmail.com>
2022-10-09 15:13:16 +0530

    Cards added and made them map over an array > Unsplash API is used for the images

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ import { PhotoCamera } from '@mui/icons-material';
 import useStyles from './styles';
 
 const App = () => {
-    const classes = useStyles();
+  const classes = useStyles();
+
+  const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   return (
     <>
       <CssBaseline />
       <AppBar position="relative">
         <Toolbar>
-          <PhotoCamera />
+          <PhotoCamera className={classes.icon} />
           <Typography variant="h6">
             Photo Album
           </Typography>
@@ -21,28 +23,53 @@ const App = () => {
       <main>
         <div className={classes.container}>
           <Container maxWidth="sm">
-              <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
-                Photo Album
-              </Typography>
-              <Typography variant='h5' align='center' color='textSecondary' paragraph>
-                Do nulla laborum voluptate dolore aute qui. Duis non ex aliqua esse ipsum exercitation qui sit qui aute. Aliquip et magna velit ea ex occaecat do ad velit cillum. Non sint sunt eu cupidatat consectetur ea in minim culpa. Aliquip dolor enim nostrud nostrud fugiat velit pariatur incididunt labore Lorem.
-              </Typography>
-              <div>
-                <Grid container spacing={2} justifyContent='center'>
-                  <Grid item>
-                    <Button variant='contained' color='primary'>
-                      See my photos
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button variant='outlined' color='primary'>
-                      Like
-                    </Button>
-                  </Grid>
+            <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
+              Photo Album
+            </Typography>
+            <Typography variant='h5' align='center' color='textSecondary' paragraph>
+              Do nulla laborum voluptate dolore aute qui. Duis non ex aliqua esse ipsum exercitation qui sit qui aute. Aliquip et magna velit ea ex occaecat do ad velit cillum. Non sint sunt eu cupidatat consectetur ea in minim culpa. Aliquip dolor enim nostrud nostrud fugiat velit pariatur incididunt labore Lorem.
+            </Typography>
+            <div className={classes.button}>
+              <Grid container spacing={2} justifyContent='center'>
+
+                <Grid item>
+                  <Button variant='contained' color='primary'>
+                    See my photos
+                  </Button>
                 </Grid>
-              </div>
+                <Grid item>
+                  <Button variant='outlined' color='primary'>
+                    Like
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
           </Container>
         </div>
+        <Container className={classes.cardGrid} maxWidth='md'>
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} >
+                <Card className={classes.card}>
+                  <CardMedia className={classes.cardMedia}
+                    image={`https://source.unsplash.com/random/${card}`} title='Image title' />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant='h5'>
+                      Heading
+                    </Typography>
+                    <Typography>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size='small' color='primary'>View</Button>
+                    <Button size='small' color='primary'>Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
       </main>
     </>
   )
