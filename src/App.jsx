5096402f2bb0e9a1803a2184c877e337d46188aa
Anirudh A V <anirudh.av02@gmail.com>
2022-10-09 15:16:08 +0530

    Grid is made responsive

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ const App = () => {
         <Container className={classes.cardGrid} maxWidth='md'>
           <Grid container spacing={4}>
             {cards.map((card) => (
-              <Grid item key={card} >
+              <Grid item key={card} xs={12} sm={6} md={4} >
                 <Card className={classes.card}>
                   <CardMedia className={classes.cardMedia}
                     image={`https://source.unsplash.com/random/${card}`} title='Image title' />
@@ -71,7 +71,7 @@ const App = () => {
           </Grid>
         </Container>
       </main>
-    </>
+      
   )
 }
 
